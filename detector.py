Fixed broken solid angle calculation

detector.py
@@ -12,7 +12,7 @@ class Detector(object):
         raise NotImplementedError
     
     def normal(self, bin_i):
-        points = self.bin_edges[[bin_i, bin_i+1]]
+        points = self.bin_edges[[bin_i, bin_i + 1]]
         # L[0].y - L[1].y
         px = points[0, 1] - points[1, 1]
         # L[1].x - L[0].x
@@ -22,13 +22,19 @@ class Detector(object):
 
     def solid_angle(self, bin_i, point):
         """Return solid angle in radians."""
-        bin_edges = self.bin_edges[[bin_i, bin_i+1]]
-        a = np.linalg.norm(point - bin_edges[0])
-        b = np.linalg.norm(point - bin_edges[1])
-        c = np.linalg.norm(bin_edges[0] - bin_edges[1])
-        arg = (a**2 + b**2 - c**2) / (2 * a * b)
-        angle = np.arccos(arg)
+        bin_edges = self.bin_edges[[bin_i, bin_i + 1]]
+        X = bin_edges[0] - point
+        Y = bin_edges[1] - point
+        X_Y = X - Y
+        cos_angle = (np.dot(X, X) + np.dot(Y, Y) - np.dot(X - Y, X - Y)) / (2 * np.linalg.norm(X) * np.linalg.norm(Y))
+        angle = np.arccos(cos_angle)
+        if angle > np.pi / 2.:
+            angle = np.pi - angle
         return angle
+
+    def solid_angles(self, point):
+        bin_solid_angles = [self.solid_angle(i, point) for i in xrange(len(self.bin_centers))]
+        return np.array(bin_solid_angles)
 
     def draw(self, normals=False):
         plt.plot(self.bin_edges[:, 0], self.bin_edges[:, 1])
@@ -66,9 +72,12 @@ class DetectorArc(Detector):
     def create_bins(self, nbins=100):
         angle_bins = np.linspace(self.angles[1], self.angles[0], nbins) * np.pi / 180.
         bins = np.zeros((nbins, 2), dtype=np.float32)
-        mean_angle_radian = sum(self.angles) / 2. * np.pi / 180.
-        center_detector = [self.radius * np.cos(mean_angle_radian), 
-                           self.radius * np.sin(mean_angle_radian)]
-        bins[:, 0] = self.radius * np.cos(angle_bins) + self.center[0] - center_detector[0]
-        bins[:, 1] = self.radius * np.sin(angle_bins) + self.center[1] - center_detector[1]
+        bins[:, 0] = self.radius * np.cos(angle_bins) + self.center[0]
+        bins[:, 1] = self.radius * np.sin(angle_bins) + self.center[1]
+
+        #mean_angle_radian = sum(self.angles) / 2. * np.pi / 180.
+        #center_detector = [self.radius * np.cos(mean_angle_radian), 
+        #                   self.radius * np.sin(mean_angle_radian)]
+        #bins[:, 0] = self.radius * np.cos(angle_bins) + self.center[0] - center_detector[0]
+        #bins[:, 1] = self.radius * np.sin(angle_bins) + self.center[1] - center_detector[1]
         return bins

plot_fission_segments.py
@@ -36,8 +36,8 @@ def build_shielded_geometry():
     small_box_2 = create_rectangle(2., 2.)
     translate_rotate_mesh(small_box_2, [6., -2.])
 
-    #sim = Simulation(air, 50., 45., 'arc')
-    sim = Simulation(air, diameter=50.,)
+    sim = Simulation(air, 50., 45., 'arc')
+    #sim = Simulation(air, diameter=50.,)
     sim.detector.width = 30.
     sim.geometry.solids.append(Solid(box, steel, air))
     sim.geometry.solids.append(Solid(hollow_circle, u235_metal, air))
@@ -75,41 +75,53 @@ def propogate_fissions_point_detector(sim, point, macro_fission):
 
     nu = 1 for now
     """
-    detector_bins = sim.detector.create_bins()
-    detector_center = (detector_bins[1:, :] + detector_bins[:-1, :]) / 2.
-    detector_width = np.linalg.norm(detector_bins[:-1] - detector_bins[:1], axis=1)
-    #detector_normal = 
-    probability = np.zeros(np.size(detector_center), 0) # use center point of each detector boundary
+    detector_solid_angle = sim.detector.solid_angles(sim.source)
 
-    start = sim.source
+    prob = detector_solid_angle
 
-    prob_atten_to_point = np.exp(-sim.attenuation_length(start, point))
-    prob_fission = macro_fission
+    return prob
 
-    prob_point_to_detector = np.zeros_like(probability)
-    for i, d_center in enumerate(detector_center):
-        prob_point_to_detector[i] = np.exp(-sim.attenuation_length(point, d_center))
+    #detector_bins = sim.detector.create_bins()
+    #detector_center = (detector_bins[1:, :] + detector_bins[:-1, :]) / 2.
+    #detector_width = np.linalg.norm(detector_bins[:-1] - detector_bins[:1],
+    #axis=1)
+    ##detector_normal =
+    #probability = np.zeros(np.size(detector_center), 0) # use center point of
+    #each detector boundary
+
+    #start = sim.source
+
+    #prob_atten_to_point = np.exp(-sim.attenuation_length(start, point))
+    #prob_fission = macro_fission
+
+    #prob_point_to_detector = np.zeros_like(probability)
+    #for i, d_center in enumerate(detector_center):
+    #    prob_point_to_detector[i] = np.exp(-sim.attenuation_length(point,
+    #    d_center))
 
     #detector_point_angle = np.dot(detector_center -
     #solid_angle = np.dot(detector_width, np.cos())
-
 def main():
     sim = build_shielded_geometry()
+    sim.detector.set_bins(100)
 
     plt.figure()
-    sim.draw()
+    sim.draw(True)
 
-    angles = np.linspace(-15., 15., 20) * np.pi / 180.
-    r = 50.
-    start = sim.source
+    plt.figure()
+    plt.plot(propogate_fissions_point_detector(sim, np.array([0., 0.]), 0.0))
 
-    for angle in angles:
-        end = start + np.array([r * np.cos(angle), r * np.sin(angle)])
+    #angles = np.linspace(-15., 15., 20) * np.pi / 180.
+    #r = 50.
+    #start = sim.source
 
-        segments, cross_sections = sim.fission_segments(start, end)
+    #for angle in angles:
+    #    end = start + np.array([r * np.cos(angle), r * np.sin(angle)])
 
-        for segment in segments:
-            plt.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], color='black')
+    #    segments, cross_sections = sim.fission_segments(start, end)
+
+    #    for segment in segments:
+    #        plt.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], color='black')
 
     plt.show()
 