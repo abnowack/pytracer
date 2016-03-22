Fixing rotation in fission response

PyTracer/simulation.py
@@ -1,15 +1,7 @@
-﻿from itertools import izip
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-from PyTracer import math2d
+﻿import matplotlib.pyplot as plt
 from PyTracer.detector import DetectorArc, DetectorPlane
 from PyTracer.geometry import Geometry
 from source import Source
-
-
-# TODO: Break out fission prob functions and recon methods
 
 
 class Simulation(object):
@@ -43,6 +35,7 @@ class Simulation(object):
             self.grid.rotate(angle)
 
     def draw(self, draw_normals=False):
+        plt.figure(figsize=(7, 7))
         self.geometry.draw(draw_normals)
 
         if self.source is not None:
@@ -54,71 +47,9 @@ class Simulation(object):
         if self.grid is not None:
             self.grid.draw_lines()
 
-        plt.axis('equal')
         plt.xlabel('X (cm)')
         plt.ylabel('Y (cm)')
-    
-    def scan(self, angles):
-        atten_length = np.zeros((self.detector.nbins, len(angles)))
-
-        self.detector.render()
-        
-        for i, angle in enumerate(angles):
-            self.rotate(angle)
-            detector_centers = math2d.center(self.detector.segments)
-            for j, detector_center in enumerate(detector_centers):
-                atten_length[j, i] = self.geometry.attenuation_length(self.source.pos, detector_center)
-        
-        return atten_length
-    
-    def radon_transform(self, angles):
-        if type(self.detector) is not DetectorPlane:
-            raise TypeError('self.detector is not DetectorPlane')
-
-        radon = np.zeros((np.size(self.detector.nbins, 0), len(angles)))
-
-        for i, angle in enumerate(angles):
-            self.rotate(angle)
-            detector_points = math2d.center(self.detector.segments)
-            source_points = np.dot(detector_points, math2d.angle_matrix(180.))[::-1]
-            for j, (source_point, detector_point) in enumerate(izip(detector_points, source_points)):
-                radon[j, i] = self.geometry.attenuation_length(source_point, detector_point)
-        
-        return radon
-
-    def propagate_fission_ray(self, start, end, n):
-        segments, macro_fissions = self.geometry.fission_segments(start, end)
-        segment_probs = []
-        for i in xrange(len(segments)):
-            single_fission_prob = self.propagate_fissions_segment(segments[i], n)
-            segment_probs.append(single_fission_prob)
-        total_fission_prob = np.sum(segment_probs, axis=0)
-        return total_fission_prob
-
-    def propagate_fissions_segment(self, segment, n=5):
-        point_0, point_1 = segment[0], segment[1]
-        # generate points along fission segment
-        # use trapezoid rule on uniform spacing
-        # int [f(x = [a, b]) dx]  ~= (b - a) / (2 * N) [ f(a) + f(b) +  ]
-        points = [point_0 + (point_1 - point_0) * t for t in np.linspace(0.01, 0.99, n)] # TODO : error if t = 1
-        values = np.zeros((len(points), len(self.detector.segments)))
-        integral = np.zeros((len(self.detector.segments)))
-        for i in xrange(len(points)):
-            values[i, :] = self.propagate_fissions_point_detector(points[i])
-        integral[:] = np.linalg.norm(point_1 - point_0) / (n - 1) * (values[0, :] + 2. * np.sum(values[1:-1, :], axis=0) + values[-1, :])
-        return integral
-
-    def propagate_fissions_point_detector(self, point):
-        """
-        Calculate probability of induced fission being detected over detector plane.
-
-        nu = 1 for now, not using macro_fission
-        """
-        detector_solid_angle = math2d.solid_angle(self.detector.segments, point) / (2. * np.pi) # returns 200,200
-        in_attenuation_length = self.geometry.attenuation_length(self.source.pos, point)
-        segment_centers = math2d.center(self.detector.segments)
-        out_attenuation_lengths = np.array([self.geometry.attenuation_length(point, center) for center in segment_centers])
-
-        prob = np.exp(-in_attenuation_length) * np.multiply(detector_solid_angle, np.exp(-out_attenuation_lengths))
-
-        return prob
+        max_lim = max(max(plt.xlim()), max(plt.ylim()))
+        plt.xlim(-max_lim, max_lim)
+        plt.ylim(-max_lim, max_lim)
+        plt.axes().set_aspect('equal', 'datalim')

scripts/plot_fission_segments.py
@@ -1,46 +1,50 @@
 ﻿import sys
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-from scripts.geometries import build_shielded_geometry
-
+from geometries import build_shielded_geometry
+from PyTracer.fission import propagate_fission_ray
 
 def main():
     sim = build_shielded_geometry(True)
 
-    plt.figure()
-
     sim.rotate(20.)
     sim.draw()
 
-    print sim.source.angle
+    angles = np.linspace(-15, 15, 50)
+    angles *= np.pi / 180.
+    for angle in angles:
+        plt.plot([sim.source.pos[0], sim.source.pos[0] + 10. * np.cos(np.pi * sim.source.angle / 180. - angle)],
+                 [sim.source.pos[1], sim.source.pos[1] + 10. * np.sin(np.pi * sim.source.angle / 180. - angle)])
 
+    plt.plot([sim.source.pos[0], - sim.source.pos[0]], [sim.source.pos[1], - sim.source.pos[1]])
 
-    angles = np.linspace(-15., 15., 50) * np.pi / 180.
-    r = 50.
-    fission_probs = np.zeros((len(angles), len(sim.detector.segments)))
+    # angles = np.linspace(-15., 15., 50)
+    # angles *= np.pi / 180.
+    #
+    # r = 50.
+    # fission_probs = np.zeros((len(angles), sim.detector.nbins))
 
-    for i, angle in enumerate(angles):
-        print i
-        end = sim.source.pos + np.array([r * np.cos(angle - sim.source.angle / 2.), r * np.sin(angle - sim.source.angle / 2.)])
+    # for i, angle in enumerate(angles):
+    #     print i
+    #     end = sim.source.pos + np.array([r * np.cos(angle - sim.source.angle / 2.), r * np.sin(angle - sim.source.angle / 2.)])
 
-        segments, cross_sections = sim.fission_segments(sim.source.pos, end)
+    #     segments, cross_sections = sim.geometry.fission_segments(sim.source.pos, end)
+    #
+    #     for segment in segments:
+    #         plt.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], color='black')
+    #
+    #     fission_probs[i, :] = propagate_fission_ray(sim, sim.source.pos, end, n=5)
+    #
+    # print np.max(fission_probs)
+    # print np.unravel_index(fission_probs.argmax(), fission_probs.shape)
 
-        for segment in segments:
-            plt.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], color='black')
-
-        fission_probs[i, :] = propagate_fission_ray(sim, sim.source.pos, end, n=5)
-
-    print np.max(fission_probs)
-    print np.unravel_index(fission_probs.argmax(), fission_probs.shape)
-
-    plt.figure()
-    plt.imshow(fission_probs.T, extent=[-15., 15., -15., 15.], interpolation='none')
-    plt.colorbar()
-    plt.xlabel('Neutron Angle')
-    plt.ylabel('Detector Bin Angle')
-    plt.title('Single Fission Detection Probability (Arb. Z Scale)')
+    # plt.figure()
+    # plt.imshow(fission_probs.T, extent=[-15., 15., -15., 15.], interpolation='none')
+    # plt.colorbar()
+    # plt.xlabel('Neutron Angle')
+    # plt.ylabel('Detector Bin Angle')
+    # plt.title('Single Fission Detection Probability (Arb. Z Scale)')
 
     plt.show()
 