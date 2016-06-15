Converted transmission response generator to use bulk attenuation calculation for speed up, looks like there is a bug though. Some pixels get a negative value for attenuation

pytracer/geometry.py
@@ -6,6 +6,7 @@ from mesh import Mesh
 from material import Material
 from itertools import izip
 
+
 class Geometry(object):
     """
     Contains all mesh objects in the simulation, then translates geometry into simple arrays for fast computation
@@ -35,7 +36,7 @@ class Geometry(object):
         """
         for solid in self.solids:
             solid.draw(draw_normals)
-    
+
     def flatten(self):
         """
         Combine all meshes into a single mesh, and all inner and outer mesh materials into single material lists
@@ -71,7 +72,7 @@ class Geometry(object):
                                               self.indexes_cache, ray)
         return self.intersects_cache[:n_intersects], self.indexes_cache[:n_intersects]
 
-    def attenuation_length(self, start, end):
+    def attenuation_length(self, start, end=None, attenuation_cache=None):
         """
         Calculate of attenuation length through geometry from start to end
 
@@ -87,10 +88,16 @@ class Geometry(object):
         atten_length : float
             calculated attenuation length
         """
-        attenuation = math2d_c.calc_attenuation(self.mesh.segments, np.array([start, end]), self.inner_attenuation,
-                                                self.outer_attenuation, self.universe_material.attenuation,
-                                                self.intersects_cache, self.indexes_cache)
-        return attenuation
+        if end is not None:
+            attenuation = math2d_c.calc_attenuation(self.mesh.segments, np.array([start, end]), self.inner_attenuation,
+                                                    self.outer_attenuation, self.universe_material.attenuation,
+                                                    self.intersects_cache, self.indexes_cache)
+            return attenuation
+        else:
+            attenuations = math2d_c.calc_attenuation_bulk(self.mesh.segments, start,
+                                                          self.inner_attenuation, self.outer_attenuation,
+                                                          self.universe_material.attenuation,
+                                                          self.intersects_cache, self.indexes_cache, attenuation_cache)
 
     def fission_segments(self, start, end):
         """
@@ -149,7 +156,7 @@ class Geometry(object):
                 elif sign < 0 and inner_material.is_fissionable:
                     segments.append([start, f_int])
                     cross_sections.append(inner_material.macro_fission)
-            elif i == len(sorted_fission_indexes)-1:
+            elif i == len(sorted_fission_indexes) - 1:
                 # test if end to last fission lixel is fissionable
                 normal = math2d.normal(self.mesh.segments[f_ind])
                 sign = np.sign(np.dot(end - f_int, normal))
@@ -169,8 +176,8 @@ class Geometry(object):
             normal_1 = math2d.normal(self.mesh.segments[f_ind])
             sign_1 = np.sign(np.dot(start - f_int, normal_1))
 
-            f_ind2 = sorted_fission_indexes[i+1]
-            f_int2 = sorted_fission_intercepts[i+1]
+            f_ind2 = sorted_fission_indexes[i + 1]
+            f_int2 = sorted_fission_intercepts[i + 1]
 
             normal_2 = math2d.normal(self.mesh.segments[f_ind2])
             sign_2 = np.sign(np.dot(start - f_int2, normal_2))

pytracer/transmission.py
@@ -9,23 +9,25 @@ from geometry import Geometry
 from solid import Solid
 
 
-def radon(sim, angles):
+def radon(sim, n_angles):
     if type(sim.detector) is not DetectorPlane:
         raise TypeError('self.detector is not DetectorPlane')
 
-    radon = np.zeros((sim.detector.nbins, len(angles)))
-
+    angles = np.linspace(0., 180., n_angles + 1)[:-1]
+    paths = np.zeros((sim.detector.nbins, len(angles), 2, 2))
     for i, angle in enumerate(angles):
         sim.rotate(angle)
-        detector_points = math2d.center(sim.detector.segments)
-        source_points = np.dot(detector_points, math2d.angle_matrix(180.))[::-1]
-        for j, (source_point, detector_point) in enumerate(izip(detector_points, source_points)):
-            radon[j, i] = sim.geometry.attenuation_length(source_point, detector_point)
+        paths[:, i, 0, :] = math2d.center(sim.detector.segments)
+        paths[:, i, 1, :] = -paths[:, i, 0, :][::-1]
 
-    return radon
+    radon_shape = (paths.shape[0], paths.shape[1])
+    radon = np.zeros((paths.shape[0] * paths.shape[1]))
+    paths = paths.reshape((paths.shape[0] * paths.shape[1], paths.shape[2], paths.shape[3]))
+    sim.geometry.attenuation_length(paths, attenuation_cache=radon)
+
+    return radon.reshape(radon_shape), angles
 
 
-# TODO : Normalization isn't correct
 def inverse_radon(radon, thetas):
     """
     Reconstruct using Filtered Back Projection.
@@ -50,7 +52,7 @@ def inverse_radon(radon, thetas):
     return reconstruction_image
 
 
-def build_transmission_response(sim, angles):
+def build_transmission_response(sim, n_angles):
     """
     Use grid to define pixel elements
 
@@ -63,39 +65,40 @@ def build_transmission_response(sim, angles):
     -------
 
     """
+    angles = np.linspace(0., 180., n_angles + 1)[:-1]
 
     if sim.grid is None:
         raise RuntimeError("Simulation must have a grid defined")
 
     response = np.zeros((sim.detector.nbins, np.size(angles), sim.grid.ncells))
+    response_shape = response.shape
+    response = response.reshape((response.shape[0] * response.shape[1], response.shape[2]))
 
     unit_material = Material(1.0, 0.0, color='black')
     vacuum_material = Material(0.0, 0.0, color='white')
     cell_geo = Geometry(universe_material=vacuum_material)
+
+    paths = np.zeros((sim.detector.nbins, len(angles), 2, 2))
+    paths[:, :, 0, :] = sim.source.pos
+    for i, angle in enumerate(angles):
+        sim.rotate(angle)
+        paths[:, i, 1, :] = math2d.center(sim.detector.segments)
+    paths = paths.reshape((paths.shape[0] * paths.shape[1], paths.shape[2], paths.shape[3]))
 
     for i in xrange(sim.grid.ncells):
         print i
         grid_square = sim.grid.create_mesh(i)
         cell_geo.solids = [Solid(grid_square, unit_material, vacuum_material)]
         cell_geo.flatten()
-        for j, angle in enumerate(angles):
-            sim.rotate(angle)
-            for k, detector_center in enumerate(math2d.center(sim.detector.segments)):
-                response[k, j, i] = cell_geo.attenuation_length(sim.source.pos, detector_center)
 
-    return response
+        cell_geo.attenuation_length(paths, attenuation_cache=response[:, i])
 
+        # for j, angle in enumerate(angles):
+        #     sim.rotate(angle)
+        #     for k, detector_center in enumerate(math2d.center(sim.detector.segments)):
+        #         response[k, j, i] = cell_geo.attenuation_length(sim.source.pos, detector_center)
 
-def scan(sim, angles):
-
-    measurement = np.zeros((sim.detector.nbins, np.size(angles)))
-
-    for i, angle in enumerate(angles):
-        sim.rotate(angle)
-        for j, detector_center in enumerate(math2d.center(sim.detector.segments)):
-            measurement[j, i] = sim.geometry.attenuation_length(sim.source.pos, detector_center)
-
-    return measurement
+    return response.reshape(response_shape)
 
 
 def recon_tikhonov(measurement, response, alpha=0.):

scripts/transmission_test.py
@@ -9,9 +9,7 @@ from geometries import build_shielded_geometry
 
 
 def radon_scan_example(sim, n_angles):
-    angles = np.linspace(0., 180., n_angles + 1)[:-1]
-
-    r = radon(sim, angles)
+    r, angles = radon(sim, n_angles)
     recon_image = inverse_radon(r, angles)
 
     extent = [-sim.detector.width / 2., sim.detector.width / 2.]
@@ -33,36 +31,27 @@ if __name__ == "__main__":
     sim = build_shielded_geometry()
     sim.grid = Grid(25, 15, 25, 15)
     sim.grid.create_mesh(225)
-    sim.detector.width = 50
-    sim.detector.nbins = 100
-    angles = np.linspace(0., 180., 50 + 1)[:-1]
+    sim.detector.width = 25
+    sim.detector.nbins = 200
 
-    # sim.rotate(20.)
+    sim.draw()
 
-    # plt.figure()
-    # sim.draw()
-    # plt.tight_layout()
+    radon_scan_example(sim, 200)
 
-    # plt.figure()
-    # trace = sim.radon_transform(([11.01]))
-    # plt.plot(trace[:, 0])
-
-    # radon_scan_example(sim, 200)
-
-    response = build_transmission_response(sim, angles)
+    # response = build_transmission_response(sim, 50)
 
     # plt.figure()
     # plt.imshow(response[:, :, 0], interpolation='none')
 
-    measurement = scan(sim, angles)
+    # measurement = scan(sim, angles)
 
     # plt.figure()
     # plt.imshow(measurement, interpolation='none')
 
-    recon = recon_tikhonov(measurement, response)
-    recon = recon.reshape(sim.grid.ny, sim.grid.nx)
+    # recon = recon_tikhonov(measurement, response)
+    # recon = recon.reshape(sim.grid.ny, sim.grid.nx)
 
     # plt.figure()
     # plt.imshow(recon, interpolation='none')
-    #
-    # plt.show()
+
+    plt.show()