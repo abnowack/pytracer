Creating single and double fission measurements, bug in attenuation calculation in which it may have a negative sign but same magnitude

pytracer/fission.py
@@ -3,7 +3,7 @@ from . import geometry as geo
 from . import transmission
 
 _fission_segment_cache = np.empty((100, 2, 2), dtype=np.double)
-_fission_value_cache = np.empty(100, dtype=np.int)
+_fission_value_cache = np.empty(100, dtype=np.double)
 
 
 def point_is_outer_segment_side(x, y, segments):
@@ -18,7 +18,7 @@ def find_fission_segments(start, end, flat_geom, fission_segments=None, fission_
         fission_values = _fission_value_cache
 
     segment_count = 0
-    intersects, indexes = transmission.intersections(start, end, flat_geom.segments)
+    intersects, indexes = transmission.intersections(np.copy(start), np.copy(end), flat_geom.segments)
     if np.size(intersects, 0) == 0:
         return fission_segments[:0], fission_values[:0]
 
@@ -45,20 +45,20 @@ def find_fission_segments(start, end, flat_geom, fission_segments=None, fission_
         if start_on_outer_side[i] and value[i, 0] > 0:
             if start_on_outer_side[i + 1] and value[i + 1, 1] > 0:
                 fission_segments[segment_count] = [intersects[i], intersects[i + 1]]
-                fission_values[segment_count] = value[i, 1]
+                fission_values[segment_count] = value[i + 1, 1]
                 segment_count += 1
             elif not start_on_outer_side[i + 1] and value[i + 1, 0] > 0:
                 fission_segments[segment_count] = [intersects[i], intersects[i + 1]]
-                fission_values[segment_count] = value[i, 1]
+                fission_values[segment_count] = value[i + 1, 0]
                 segment_count += 1
         elif not start_on_outer_side[i] and value[i, 0] > 0:
-            if start_on_outer_side[i + 1] and flat_geom.fission[i + 1, 1] > 0:
+            if start_on_outer_side[i + 1] and value[i + 1, 1] > 0:
                 fission_segments[segment_count] = [intersects[i], intersects[i + 1]]
-                fission_values[segment_count] = value[i, 1]
+                fission_values[segment_count] = value[i + 1, 1]
                 segment_count += 1
             elif not start_on_outer_side[i + 1] and value[i + 1, 0] > 0:
                 fission_segments[segment_count] = [intersects[i], intersects[i + 1]]
-                fission_values[segment_count] = value[i, 1]
+                fission_values[segment_count] = value[i + 1, 0]
                 segment_count += 1
 
     # test if [intersect[-1], end] is fissionable path
@@ -72,3 +72,163 @@ def find_fission_segments(start, end, flat_geom, fission_segments=None, fission_
         segment_count += 1
 
     return fission_segments[:segment_count], fission_values[:segment_count]
+
+
+def break_segment(segment, num_points=5):
+    points = np.zeros((num_points, 2), dtype=segment.dtype)
+    points[:, 0] = np.linspace(segment[0, 0], segment[1, 0], num_points + 2)
+    points[:, 1] = np.linspace(segment[0, 1], segment[1, 1], num_points + 2)
+    return points
+
+
+def probability_detect(fission_position, flat_geom, detector_segments):
+    # calc attenuation from fission position to center of detector segments
+    # could calc the average over a segment if attenuation varies enough over the segment
+    end_positions = geo.center(detector_segments)
+    start_positions = np.tile(fission_position, (len(end_positions), 1))
+    out_attenuations = np.abs(transmission.attenuations(start_positions, end_positions, flat_geom.segments,
+                                                        flat_geom.attenuation))
+    exit_prob = np.exp(-out_attenuations)
+
+    # calc solid angle of detector from fission_point
+    solid_angles = geo.solid_angle(detector_segments, fission_position)
+    prob_solid_angle = solid_angles / (2 * np.pi)
+
+    prob_detect = np.sum(prob_solid_angle * exit_prob)
+
+    return prob_detect
+
+
+def probability_in(source, fission_position, flat_geom):
+    # TODO: FIX
+    prob_atten = np.exp(
+        -np.abs(transmission.attenuation(source, fission_position, flat_geom.segments, flat_geom.attenuation)))
+    # prob_atten = transmission.attenuation(source, fission_position, flat_geom.segments, flat_geom.attenuation)
+    return prob_atten
+
+
+def probability_out_single(fission_position, flat_geom, detector_segments, avg_nu):
+    prob_detect = probability_detect(fission_position, flat_geom, detector_segments)
+    prob_out_single = np.exp(-prob_detect * avg_nu) * prob_detect * avg_nu
+    return np.abs(prob_out_single)  # TODO: FIX
+
+
+def probability_per_ds_neutron_single(source, fission_position, flat_geom, mu_fission, detector_segments, avg_nu):
+    prob_in = probability_in(source, fission_position, flat_geom)
+    prob_out = probability_out_single(fission_position, flat_geom, detector_segments, avg_nu)
+    prob_per_ds_fission = mu_fission
+
+    return prob_in * prob_per_ds_fission * prob_out
+
+
+def probability_segment_neutron_single(source, fission_segment, mu_fission, flat_geom, detector_segments, avg_nu,
+                                       num_segment_points=5):
+    fission_positions = np.zeros((num_segment_points, 2), dtype=np.double)
+    epsilon = 1.0e-6
+    x_start, x_end = epsilon * fission_segment[1, 0] + (1 - epsilon) * fission_segment[0, 0], \
+                     (1 - epsilon) * fission_segment[1, 0] + epsilon * fission_segment[0, 0],
+    y_start, y_end = epsilon * fission_segment[1, 1] + (1 - epsilon) * fission_segment[0, 1], \
+                     (1 - epsilon) * fission_segment[1, 1] + epsilon * fission_segment[0, 1],
+
+    fission_positions[:, 0] = np.linspace(x_start, x_end, num_segment_points)
+    fission_positions[:, 1] = np.linspace(y_start, y_end, num_segment_points)
+
+    segment_length = np.sqrt(
+        (fission_segment[0, 0] - fission_segment[1, 0]) ** 2 + (fission_segment[0, 1] - fission_segment[1, 1]))
+
+    segment_probability = 0.
+
+    for fission_position in fission_positions:
+        prob_ds = probability_per_ds_neutron_single(source, fission_position, flat_geom, mu_fission, detector_segments,
+                                                    avg_nu)
+        segment_probability += prob_ds
+
+    segment_probability *= segment_length
+
+    return segment_probability
+
+
+def probability_path_neutron_single(start, end, flat_geom, detector_segments, avg_nu):
+    segments, values = find_fission_segments(start, end, flat_geom)
+
+    prob = 0
+    for (segment, value) in zip(segments, values):
+        prob += probability_segment_neutron_single(start, segment, value, flat_geom, detector_segments, avg_nu)
+
+    return prob
+
+
+def probability_out_double(fission_position, flat_geom, detector_segments, avg_nu):
+    prob_detect = probability_detect(fission_position, flat_geom, detector_segments)
+    prob_out_double = 0.5 * np.exp(-prob_detect * avg_nu) * (prob_detect * avg_nu) ** 2
+    return prob_out_double
+
+
+def probability_per_ds_neutron_double(source, fission_position, flat_geom, mu_fission, detector_segments, avg_nu):
+    prob_in = probability_in(source, fission_position, flat_geom)
+    prob_out = probability_out_double(fission_position, flat_geom, detector_segments, avg_nu)
+    prob_per_ds_fission = mu_fission
+
+    return prob_in * prob_per_ds_fission * prob_out
+
+
+def probability_segment_neutron_double(source, fission_segment, mu_fission, flat_geom, detector_segments, avg_nu,
+                                       num_segment_points=5):
+    fission_positions = np.zeros((num_segment_points, 2), dtype=np.double)
+    epsilon = 1.0e-6
+    x_start, x_end = epsilon * fission_segment[1, 0] + (1 - epsilon) * fission_segment[0, 0], \
+                     (1 - epsilon) * fission_segment[1, 0] + epsilon * fission_segment[0, 0],
+    y_start, y_end = epsilon * fission_segment[1, 1] + (1 - epsilon) * fission_segment[0, 1], \
+                     (1 - epsilon) * fission_segment[1, 1] + epsilon * fission_segment[0, 1],
+
+    fission_positions[:, 0] = np.linspace(x_start, x_end, num_segment_points)
+    fission_positions[:, 1] = np.linspace(y_start, y_end, num_segment_points)
+
+    segment_length = np.sqrt(
+        (fission_segment[0, 0] - fission_segment[1, 0]) ** 2 + (fission_segment[0, 1] - fission_segment[1, 1]))
+
+    segment_probability = 0.
+
+    for fission_position in fission_positions:
+        prob_ds = probability_per_ds_neutron_double(source, fission_position, flat_geom, mu_fission, detector_segments,
+                                                    avg_nu)
+        segment_probability += prob_ds
+
+    segment_probability *= segment_length
+
+    return segment_probability
+
+
+def probability_path_neutron_double(start, end, flat_geom, detector_segments, avg_nu):
+    segments, values = find_fission_segments(start, end, flat_geom)
+
+    prob = 0
+    for (segment, value) in zip(segments, values):
+        prob += probability_segment_neutron_double(start, segment, value, flat_geom, detector_segments, avg_nu)
+
+    return prob
+
+
+def grid_response_single(start, end, grid, flat_geom, detector_segments, avg_nu):
+    """ Return array of size grid containing single fission response"""
+
+    unit_m = geo.Material('black', 1, 1)
+    vacuum = geo.Material('white', 0, 0)
+
+    flat_start = start.reshape(-1, start.shape[-1])
+    flat_end = end.reshape(-1, end.shape[-1])
+
+    response = np.zeros((grid.num_cells,) + start.shape[:-1])
+    response_shape = response.shape
+    response = response.reshape((response.shape[0], response.shape[1] * response.shape[2]))
+
+    for i in range(grid.num_cells):
+        cell_geom = [geo.Solid(geo.convert_points_to_segments(grid.cell(i)), unit_m, vacuum)]
+        cell_flat = geo.flatten(cell_geom)
+
+        for j, (s, e) in enumerate(zip(start, end)):
+            segments, values = find_fission_segments(s, e, cell_flat)
+            response[i, j] = probability_segment_neutron_single(s, segments[0], values[0], flat_geom, detector_segments,
+                                                                avg_nu)
+
+    return response.reshape(response_shape)

pytracer/geometry.py
@@ -27,7 +27,7 @@ def solid_angle(segments, point):
     b = np.linalg.norm(segments[:, 1] - point, axis=1)
     c = np.linalg.norm(segments[:, 0] - segments[:, 1], axis=1)
 
-    num = a ** 2 + b ** 2 + c ** 2
+    num = a ** 2 + b ** 2 - c ** 2
     denom = 2 * a * b
     angle = np.arccos(np.abs(num / denom))
     is_greater_halfpi = angle > np.pi / 2

scripts/transmission_response_test.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     radians = np.linspace(0, np.pi, 100)
     start, end, extent = geo.parallel_beam_paths(height=30, num_projections=200, offset=30, radians=radians,
                                                  extent=True)
-    for (s, e) in zip(start[:, 0], end[:, 0]):
-        plt.plot([s[0], e[0]], [s[1], e[1]], color='blue')
+    # for (s, e) in zip(start[:, 0], end[:, 0]):
+    #     plt.plot([s[0], e[0]], [s[1], e[1]], color='blue')
 
     measurement = transmission.scan(assembly_flat, start, end)
 