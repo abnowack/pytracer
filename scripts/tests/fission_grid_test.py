f51d35849b801e9aa3a65bb0d02c38cc9ea34aeb
Aaron Nowack <aaronbnowack@gmail.com>
2016-07-26 16:45:37 -0400

    Refactoring tests into test folder

--- scripts/tests/fission_grid_test.py
+++ scripts/tests/fission_grid_test.py
@@ -32,37 +32,33 @@ if __name__ == "__main__":
     # plt.imshow(response_single[cell_i].T, interpolation='none', extent=extent)
     # plt.title('Single Fission Response')
     #
-    # arc_rad_i, rad_i = 31, 30
+    arc_rad_i, rad_i = 31, 30
 
     unit_m = geo.Material('black', 1, 1)
     vacuum = geo.Material('white', 0, 0)
     cell_geom = [geo.Solid(geo.convert_points_to_segments(grid.cell(cell_i), True), unit_m, vacuum)]
     cell_flat = geo.flatten(cell_geom)
-    # detector_segments = geo.convert_points_to_segments(detector_points[:, rad_i])
-    # result = fission.grid_cell_response_single(source[rad_i], detector_points[arc_rad_i, rad_i], detector_segments,
-    #                                            cell_flat, assembly_flat, 0.2)
-    # print(result)
+    detector_segments = geo.convert_points_to_segments(detector_points[:, rad_i])
+    result = fission.grid_cell_response_single(source[rad_i], detector_points[arc_rad_i, rad_i], detector_segments,
+                                               cell_flat, assembly_flat, 0.2)
+    print(result)
 
     plt.figure()
     geo.draw(assembly_solids)
-    # plt.scatter(source[rad_i, 0], source[rad_i, 1])
-    # plt.plot(detector_points[:, rad_i, 0], detector_points[:, rad_i, 1])
-    # plt.plot([source[rad_i, 0], detector_points[arc_rad_i, rad_i, 0]],
-    #          [source[rad_i, 1], detector_points[arc_rad_i, rad_i, 1]])
+    plt.scatter(source[rad_i, 0], source[rad_i, 1])
+    plt.plot(detector_points[:, rad_i, 0], detector_points[:, rad_i, 1])
+    plt.plot([source[rad_i, 0], detector_points[arc_rad_i, rad_i, 0]],
+             [source[rad_i, 1], detector_points[arc_rad_i, rad_i, 1]])
     plt.scatter(grid_points[:, 0], grid_points[:, 1], zorder=12)
 
-    # segment, val = fission.find_fission_segments(source[rad_i], detector_points[arc_rad_i, rad_i], cell_flat)
-    # segment = segment[0]
-    # plt.plot([segment[0, 0], segment[1, 0]], [segment[0, 1], segment[1, 1]], color='black')
-    # plt.scatter([segment[0, 0], segment[1, 0]], [segment[0, 1], segment[1, 1]], color='black')
-    #
-    # intersects, indexes = transmission.intersections(source[rad_i], detector_points[arc_rad_i, rad_i],
-    #                                                  cell_flat.segments)
-    # plt.scatter(intersects[:, 0], intersects[:, 1], color='red')
-    geo.draw(cell_geom, True)
+    segment, val = fission.find_fission_segments(source[rad_i], detector_points[arc_rad_i, rad_i], cell_flat)
+    segment = segment[0]
+    plt.plot([segment[0, 0], segment[1, 0]], [segment[0, 1], segment[1, 1]], color='black')
+    plt.scatter([segment[0, 0], segment[1, 0]], [segment[0, 1], segment[1, 1]], color='black')
 
-    print(grid.cell(cell_i))
-    print(cell_flat.segments)
-    # print(segment)
+    intersects, indexes = transmission.intersections(source[rad_i], detector_points[arc_rad_i, rad_i],
+                                                     cell_flat.segments)
+    plt.scatter(intersects[:, 0], intersects[:, 1], color='red')
+    # geo.draw(cell_geom, True)
 
     plt.show()
